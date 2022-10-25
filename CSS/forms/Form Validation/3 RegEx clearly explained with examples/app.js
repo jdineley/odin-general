4df2595cc10e606eb61aed2eb6e18240fb3960b0
James Dineley <jdineley@hotmail.com>
2022-10-25 23:08:05 +0100

    Update

--- CSS/forms/Form Validation/3 RegEx clearly explained with examples/app.js
+++ CSS/forms/Form Validation/3 RegEx clearly explained with examples/app.js
@@ -19,18 +19,11 @@ let numSimplified = phoneNums.map((num) => {
 
 let people = ['Smith, Mr. John', 'Davis, Ms Nicole', 'Robinson, Mrs. Rebecca', 'Armstrong, Dr Sam', 'Downey, Mr. Robert'];
 
-// let target = [
-//     {
-//         title: 'Mr',
-//         firstname: 'John',
-//         lastname: 'Smith'
-//     }
-// ]
-
-//match (\D+)\, (mr\.?|mrs\.?|dr\.?|ms\.?) (\D+)
-let regex2 = /([a-z]+)\,\s(mr|mrs|dr|ms)\.?\s([a-z]+)/i
-//test ^\D+\, [md]rs?.? \D+$
+// Phone numbering verification
 let regex3 = /^[a-z]+\,\s[md]r?s?.?\s[a-z]+$/i
+
+// Phone group capturing
+let regex2 = /([a-z]+)\,\s(mr|mrs|dr|ms)\.?\s([a-z]+)/i
 
 
 let namesExtract = people.map((person) => {
@@ -56,31 +49,12 @@ let namesExtract = people.map((person) => {
 
 let urls = ['https://www.google.com/gmail', 'http://www.medium.com', 'https://twitter.com/home'];
 
+// url verification
 let regex4 = /^https?:\/\/(w{3}\.)?\w+\.\w+\/?\w*$/
+// url group capturing
 let regex5 = /(https?):\/\/(www)?\.?(\w+)\.(\w+)\/?(\w+)?/
-// let pieces = [
-//     {
-//         type: 'schema',
-//         regex: /https?/
-//     },
-//     {
-//         type: 'subdomain',
-//         regex: /www/
-//     },
-//     {
-//         type: 'second_level_domain',
-//         regex: /\w+/
-//     },
-//     {
-//         type: 'top_level_domain',
-//         regex: /\w+/
-//     },
-//     {
-//         type: 'subdirectory',
-//         regex: /\w+/
-//     }
-// ]
-let pieces = ['schema', 'subdomain', 'second-level-domain', 'top-level-domain', 'subdirectory']
+
+let urlPieces = ['schema', 'subdomain', 'second-level-domain', 'top-level-domain', 'subdirectory']
 
 // let urlSeparated = urls[0].match(regex5);
 
@@ -95,14 +69,77 @@ let urlExtract = urls.map((url) => {
         // console.log(urlSeparated.length);
         for(let i = 1; i < urlSeparated.length; i++){
             if(urlSeparated[i]){
-                urlObj[pieces[i-1]] = urlSeparated[i]
+                urlObj[urlPieces[i-1]] = urlSeparated[i]
             } else {
-                urlObj[pieces[i-1]] = 'NA'
+                urlObj[urlPieces[i-1]] = 'NA'
             }
         }
     return urlObj;
     } else {
         urlObj.invalid = 'YES'
         return urlObj;
+    }
+})
+
+let emails = ['jasonchong_98@hotmail.com', 'jason-chong-14@unimelb.edu.au','Jason.Chong@quantium.com.au'];
+let emailPieces = ['username', 'domain_name', 'domain'];
+
+// email form verification
+let regex6 = /^[a-z0-9\-\_\.]+@[a-z]+\.[a-z]+\.?[a-z]*$/i
+// email group capturing
+let regex7 = /([a-z0-9\-\_\.]+)@([a-z]+)\.([a-z]+\.?[a-z]*)/i
+
+let emailExtract = emails.map((email) => {
+    let emailObj = {email: email};
+    if(regex6.test(email)){
+        emailObj.invalid = 'NO';
+        let emailSeparated = email.match(regex7);
+        console.log(emailSeparated);
+        for(let i = 1; i < emailSeparated.length; i++){
+            if(emailSeparated[i]){
+                emailObj[emailPieces[i-1]] = emailSeparated[i]
+            } else {
+                emailObj[emailPieces[i-1]] = 'NA'
+            }
+        }
+        return emailObj;
+    } else {
+        emailObj.invalid = 'YES'
+        return emailObj;
+    }
+})
+
+
+let addresses = ['21 Bungana Drive, Kybunga SA 5453',
+ 'Thomas Lane, Fitzroy North VIC 3068', 
+ '107 Quayside Vista, Kingston ACT 2604', 
+ '94 Prince Street, Lower Coldstream NSW 2460', 
+ 'George Street, Brisbane QLD'];
+
+
+let addressPieces = ['house_number', 'street_name', 'suburb', 'state', 'postcode'];
+
+// Address verification
+let regex8 = /^[0-9a-zA-Z\s]+\,\s[a-zA-Z\s]+\s[A-Z]+\s?[0-9]*/
+// Address group capture
+let regex9 = /([0-9]*)\s([0-9a-zA-Z\s]+)\,\s([a-zA-Z\s]+)\s([A-Z]+)\s?([0-9]*)/
+
+let addressExtract = addresses.map((address) => {
+    let addressObj = {address: address};
+    if(regex8.test(address)){
+        addressObj.invalid = 'NO';
+        let addressSeparated = address.match(regex9);
+        console.log(addressSeparated);
+        for(let i = 1; i < addressSeparated.length; i++){
+            if(addressSeparated[i]){
+                addressObj[addressPieces[i-1]] = addressSeparated[i]
+            } else {
+                addressObj[addressPieces[i-1]] = 'NA'
+            }
+        }
+        return addressObj;
+    } else {
+        addressObj.invalid = 'YES'
+        return addressObj;
     }
 })
